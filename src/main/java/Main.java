b6986afc6523f453dd0987773e88eb721a52a5b6
leet5 <leet5@mail.ru>
2017-11-27 15:51:23 +0300

    Common code

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.Frontend;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/mirror");
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
